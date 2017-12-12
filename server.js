df4937622884231d9c712ec368762f5a36c0541b
tskon <beatxd@gmail.com>
2017-12-12 17:58:32 +0300

    Node.js: echo server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var url = require('url');
 
-var server = new http.Server();
+var server = new http.Server(function(req, res){
+  console.log(req.method, req.url);
+
+  var urlParsed = url.parse(req.url, true);
+
+  if (urlParsed.pathname === '/echo' && urlParsed.query.message){
+    res.end('All right');
+  }else{
+    res.end('Error 404: Page not found');
+  }
+  console.log(urlParsed);
+
+});
 
 server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-server.on('request', function (req, res) {
-  res.end('Hello world ' + ++counter);
-});
