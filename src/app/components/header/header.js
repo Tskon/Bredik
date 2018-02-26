8e07126684bd4f92ad169bc7ce0b39ae6400a81f
tskon <beatxd@gmail.com>
2018-02-26 19:01:16 +0300

    fix main page.

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
         <div className="col-9 offset-md-1 header__text">
           <h1 className='header__project-name'><a href='#'>Bredik</a></h1>
           <h5 className='header__project-description'>Сборник готовых решений для разработчика</h5></div>
-        <div className="col-2 header__login-btn-wrapper">
+        <div className="col-md-2 col-sm-6 header__login-btn-wrapper">
           <a href='/admin' className='btn btn-outline-primary'>Войти</a>
         </div>
       </section>
