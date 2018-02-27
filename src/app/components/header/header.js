0d65079ba154ca355b09e41a42bcb24c1c243614
tskon <beatxd@gmail.com>
2018-02-27 16:01:52 +0300

    add main menu

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <section className='header row justify-content-center'>
-        <div className="col-9 offset-md-1 header__text">
+        <div className="col-sm-12 col-md-9 offset-md-1 header__text">
           <h1 className='header__project-name'><a href='#'>Bredik</a></h1>
           <h5 className='header__project-description'>Сборник готовых решений для разработчика</h5></div>
         <div className="col-md-2 col-sm-6 header__login-btn-wrapper">
--- src/app/components/menu/main-menu.js
+++ src/app/components/menu/main-menu.js
@@ -3,8 +3,20 @@ import React from 'react';
 export default class Menu extends React.Component {
   render() {
     return (
-      <nav>
-        Пункты меню
+      <nav className="navbar bg-primary main-menu">
+        <span className="navbar-brand">Категории модулей</span>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">HTML/CSS/JS</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Joomla</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Wordpress</a>
+          </li>
+
+        </ul>
       </nav>
     )
   }
