c2ffba5205418678d4684faf587114431a7ab0bb
tskon <beatxd@gmail.com>
2018-02-27 16:21:14 +0300

    upd main-menu styles

--- src/app/components/menu/main-menu.js
+++ src/app/components/menu/main-menu.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Menu extends React.Component {
   render() {
     return (
-      <nav className="navbar bg-primary main-menu">
-        <span className="navbar-brand">Категории модулей</span>
+      <nav className="navbar bg-primary main-menu col-md-6 col-sm-12">
+        <div className="navbar-brand">Категории модулей</div>
         <ul className="nav nav-tabs">
           <li className="nav-item">
             <a className="nav-link active" href="#">HTML/CSS/JS</a>
