9a5e6fcc0a4e39532659d4596f6c4899c149c446
tskon <beatxd@gmail.com>
2018-03-08 00:43:48 +0300

    start connecting 'passport' to project

--- backend/model/passport.js
+++ backend/model/passport.js
@@ -31,3 +31,40 @@ passport.deserializeUser(function(id, done) {
       : done(null,user);
   });
 });
+
+
+module.exports.login = function(req, res, next) {
+  passport.authenticate('local',
+    function(err, user, info) {
+      return err
+        ? next(err)
+        : user
+          ? req.logIn(user, function(err) {
+            return err
+              ? next(err)
+              : res.redirect('/private');
+          })
+          : res.redirect('/');
+    }
+  )(req, res, next);
+};
+
+
+module.exports.logout = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
+
+// Регистрация пользователя. Создаем его в базе данных, и тут же, после сохранения, вызываем метод `req.logIn`, авторизуя пользователя
+module.exports.register = function(req, res, next) {
+  var user = new User({ username: req.body.email, password: req.body.password});
+  user.save(function(err) {
+    return err
+      ? next(err)
+      : req.logIn(user, function(err) {
+        return err
+          ? next(err)
+          : res.redirect('/private');
+      });
+  });
+};
