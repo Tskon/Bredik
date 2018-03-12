677c1bf455128f7211421a4091bde76b0495b0be
tskon <beatxd@gmail.com>
2018-03-12 15:30:07 +0300

    Passport.js: integration done. Need create some logic for login\logout\admin

--- backend/model/auth.js
+++ backend/model/auth.js
@@ -1,3 +1,37 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+const authConfig = require('../config/auth');
 
-module.exports = passport;
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy(
+  // Use the API access settings stored in ../config/auth.json. You must create
+  // an OAuth 2 client ID and secret at: https://console.developers.google.com
+  authConfig.google,
+
+  function (accessToken, refreshToken, profile, done) {
+    // Typically you would query the database to find the user record
+    // associated with this Google profile, then pass that object to the `done`
+    // callback.
+    return done(null, profile);
+  }
+));
+
+// Simple route middleware to ensure user is authenticated.
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // if auth failed
+  res.redirect('/');
+}
+
+module.exports.passport = passport;
+module.exports.isAuth = isAuth;
--- server.js
+++ server.js
@@ -2,48 +2,25 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-const authConfig = require('./backend/config/auth');
+const auth = require('./backend/model/auth');
 const serverCfg = require('./backend/config/server');
 const renderPage = require('./backend/model/render-page');
 
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new GoogleStrategy(
-  // Use the API access settings stored in ../config/auth.json. You must create
-  // an OAuth 2 client ID and secret at: https://console.developers.google.com
-  authConfig.google,
-
-  function(accessToken, refreshToken, profile, done) {
-    // Typically you would query the database to find the user record
-    // associated with this Google profile, then pass that object to the `done`
-    // callback.
-    return done(null, profile);
-  }
-));
-
 const app = express();
 
-app.use(cookieParser());
-// .use(bodyParser.urlencoded({extended: false}))
-// .use(bodyParser.json())
+app.use(cookieParser())
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(bodyParser.json());
 app.use(session({
   secret: 'bredik-secret-some-small-key',
   resave: false,
   saveUninitialized: false
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(auth.passport.initialize());
+app.use(auth.passport.session());
 
 app.use(express.static(serverCfg.publicPath));
 
@@ -54,16 +31,16 @@ app.get('/', (req, res) => {
   renderPage('js/app.js', req, res);
 });
 
-app.get('/admin', ensureAuthenticated, (req, res) => {
+app.get('/admin', auth.isAuth, (req, res) => {
   res.send(req.user);
   // renderPage('js/admin.js', req, res);
 });
 
 app.get('/auth/google',
-  passport.authenticate('google', {scope: ['openid email profile']}));
+  auth.passport.authenticate('google', {scope: ['openid email profile']}));
 
 app.get('/auth/google/callback',
-  passport.authenticate('google', {
+  auth.passport.authenticate('google', {
     failureRedirect: '/'
   }),
   function (req, res) {
@@ -80,11 +57,3 @@ app.listen(process.env.PORT || serverCfg.PORT);
 
 console.log('server was started: http://localhost:' + serverCfg.PORT);
 
-
-// Simple route middleware to ensure user is authenticated.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
