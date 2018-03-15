478779516f5254a47d9fd46e8d36952aef62bd47
Tatiana.Volkova <br-bred@yandex.ru>
2018-03-15 16:27:29 +0300

    upd index-content

--- src/app/components/content/solution-page/preview.js
+++ src/app/components/content/solution-page/preview.js
@@ -3,9 +3,24 @@ import React from 'react';
 export default class SolutionPreview extends React.Component {
   render() {
     return (
-      <div>
 
-      </div>
+          <div className="col-md-4 solution-page-preview">
+            <div className="card mb-4 box-shadow">
+              <div className="card-body">
+                <img src="img/cube.jpg" className="card-img-top img-responsive" alt="Responsive image"></img>
+                <p className="card-text">Адаптивный слайдер на чистом JS
+                </p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="btn-group">
+                    <button type="button" className="btn btn-sm btn-outline-secondary">ДЕМО</button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Скачать</button>
+                  </div>
+                  <small className="text-muted">JavaScript</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
     )
   }
 }
--- src/app/layouts/index-content.js
+++ src/app/layouts/index-content.js
@@ -5,10 +5,11 @@ import SolutionPagePreview from '../components/content/solution-page/preview';
 export default class Index extends React.Component {
   render() {
     return (
-      <div className="raw">
+      <div className="row index-content">
         <SolutionPagePreview id={1}/>
         <SolutionPagePreview id={2}/>
         <SolutionPagePreview id={3}/>
+        <SolutionPagePreview id={4}/>
       </div>
     )
   }
