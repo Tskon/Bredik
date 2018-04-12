144a0186fea5e24a8bddd6c9a8310692db989e9a
tskon <beatxd@gmail.com>
2018-04-12 19:43:02 +0300

    replace admin in main app: del admin file-branch.

--- src/admin/admin-app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-require('bootstrap');
-
-// Layouts
-import Index from '~/admin/layouts/index';
-
-ReactDOM.render(<Index/>, document.querySelector('main'));
