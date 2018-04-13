2eba07a21b3a93bdf63c3360a9cb2b40400543fe
tskon <beatxd@gmail.com>
2018-04-13 23:09:33 +0300

    add codepen iframe on demo page

--- src/app/components/content/solution-page/preview.js
+++ src/app/components/content/solution-page/preview.js
@@ -8,7 +8,7 @@ export default class SolutionPreview extends React.Component {
       <div className="col-sm-6 col-md-4 col-lg-3 ">
         <div className="card mb-4 box-shadow">
           <div className="card-body">
-            <img src="img/cube.jpg" className="card-img-top img-responsive" alt="Responsive image"></img>
+            <img src="img/cube.jpg" className="card-img-top img-responsive" alt="Responsive image"/>
             <p className="card-text">Адаптивный слайдер на чистом JS
             </p>
             <div className="d-flex justify-content-between align-items-center">
