985c259f368b99daa07ee37b96e71d4d3b7e7f6a
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-04-15 01:26:54 +0300

    add redux

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import store from '~/app/store';
 
 require('bootstrap');
 
@@ -9,12 +10,12 @@ import Main from '~/app/layouts/main';
 import AdminMain from '~/app/layouts/admin-main';
 
 ReactDOM.render(
-  //<Provider store={store}>
-  <BrowserRouter>
-    <Switch>
-      <Route path='/admin' component={AdminMain}/>
-      <Route path='/' component={Main}/>
-    </Switch>
-  </BrowserRouter>
-  // </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/admin' component={AdminMain} />
+        <Route path='/' component={Main} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
   , document.querySelector('main'));
