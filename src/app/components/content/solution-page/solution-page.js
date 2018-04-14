16cd1f0b4c1f0b5126b7ceadb92b5283a66dcab2
tskon <beatxd@gmail.com>
2018-04-14 20:51:56 +0300

    cut codepen structure

--- src/app/components/content/solution-page/solution-page.js
+++ src/app/components/content/solution-page/solution-page.js
@@ -8,14 +8,9 @@ export default class SolutionPage extends React.Component {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cum enim maiores omnis rem tenetur? Dicta
           dolores ducimus excepturi explicabo facilis hic inventore mollitia natus nemo placeat, porro ut vitae.</p>
         <div className="codepen-wrapper">
-          <iframe height='235' scrolling='no' title='Some pen'
+          <iframe scrolling='no' title='Some pen'
                   src='//codepen.io/TsKon/embed/EEzBbZ/?height=235&theme-id=0&default-tab=css,result&embed-version=2'
-                  frameBorder='no' allowFullScreen='true'>
-            See the Pen
-            <a href='https://codepen.io/TsKon/pen/EEzBbZ/'>Some pen</a>
-            by Konstantin Tsekhmeister (<a href='https://codepen.io/TsKon'>@TsKon</a>) on
-            <a href='https://codepen.io'>CodePen</a>.
-          </iframe>
+                  frameBorder='no' allowFullScreen='true'/>
         </div>
       </div>
     )
