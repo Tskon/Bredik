11ab3debca375dfc579499da906d435273c2e94f
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-04-15 02:37:56 +0300

    fill get-solution handler

--- src/app/redusers/solution-reduser.js
+++ src/app/redusers/solution-reduser.js
@@ -1,9 +1,27 @@
-export function getSolutions (state = {}, action){
-  switch (action.type){
-    case 'SET_STATE': {
-      state = {...state, appState: action.payload};
+export function getSolutions(state = {solutions: [], isFulfilled: false}, action) {
+
+  switch (action.type) {
+
+    case 'GET_SOLUTIONS_PENDING': {
       break;
     }
+
+    case 'GET_SOLUTIONS_FULFILLED': {
+      state = {
+        ...state,
+        isFulfilled: true,
+        solutions: action.payload
+      };
+      break;
+    }
+
+    case 'GET_SOLUTIONS_REJECTED': {
+      state = {
+        ...state,
+        isFulfilled: false
+      }
+    }
   }
+
   return state;
 }
