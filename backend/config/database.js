2773bb2637211bac7f2e5caaf96f2050c7ac8240
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-04-16 01:11:54 +0300

    change db settings

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const PORT = 80;
 const publicPath = 'dist/';
 
-const dbHost = 'localhost';
+const dbHost = 'beatxdfy.beget.tech';
 const dbName = 'beatxdfy_bredik';
 const dbUser = 'beatxdfy_bredik';
-const dbPass = 'bredik';
+const dbPass = 'q1w2Q!W@';
 
 module.exports = {
   PORT,
