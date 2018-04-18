e5c91a28e8a8ac2684be82b944dd5dca3dab05fd
tskon <beatxd@gmail.com>
2018-04-18 23:32:38 +0300

    clear styles

--- src/app/components/menu/main-menu.js
+++ src/app/components/menu/main-menu.js
@@ -3,59 +3,18 @@ import {Link} from 'react-router-dom';
 
 export default class Menu extends React.Component {
   setActive(href) {
-    return (this.props.path === href) ? 'active' : '';
+    return (this.props.path === href) ? 'sections-menu__item_active' : '';
   }
 
   render() {
     return (
-      <nav className="navbar main-menu navbar-expand-md">
-        <div className="container">
-          {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>*/}
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="nav nav-tabs nav-justified navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/")} to="/">HTML/CSS</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/javascript")} to="/javascript">JavaScript</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/wp")} to="/wp">Wordpress</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/joomla")} to="/joomla">Joomla</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/drupal")} to="/drupal">Drupal</Link>
-            </li>
-          </ul>
-          </div>
-      </div>
+      <nav className="sections-menu">
+        <Link className={"sections-menu__item " + this.setActive("/")} to="/">HTML/CSS/JS</Link>
+        <Link className={"sections-menu__item " + this.setActive("/javascript")} to="/javascript">JavaScript</Link>
+        <Link className={"sections-menu__item " + this.setActive("/wp")} to="/wp">Wordpress</Link>
+        <Link className={"sections-menu__item " + this.setActive("/joomla")} to="/joomla">Joomla</Link>
+        <Link className={"sections-menu__item " + this.setActive("/drupal")} to="/drupal">Drupal</Link>
       </nav>
-      /*<nav className="navbar bg-primary main-menu col-md-6 col-sm-12">
-        <div className="container">
-          <ul className="nav nav-tabs nav-justified">
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/")} to="/">HTML/CSS</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/javascript")} to="/javascript">JavaScript</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/wp")} to="/wp">Wordpress</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/joomla")} to="/joomla">Joomla</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + this.setActive("/drupal")} to="/drupal">Drupal</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>*/
     )
   }
 }
