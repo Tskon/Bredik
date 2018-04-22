c700f02c73f1b24f0c9d456bb622e6342ce8674b
tskon <beatxd@gmail.com>
2018-04-22 11:56:40 +0300

    change bd hosting

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,6 @@
-const PORT = 80;
-const publicPath = 'dist/';
-
-const dbHost = 'beatxdfy.beget.tech';
-const dbName = 'beatxdfy_bredik';
-const dbUser = 'beatxdfy_bredik';
+const dbHost = 'tskon.beget.tech';
+const dbName = 'tskon_bredik';
+const dbUser = 'tskon_bredik';
 const dbPass = 'q1w2Q!W@';
 
 module.exports = {
