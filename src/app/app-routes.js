da96af7c43caad7d7a1b734807149ecf44d2eccd
tskon <tsekhmeister.k@gmail.com>
2018-05-05 19:44:46 +0300

    replace routes

---
+++ src/app/app-routes.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {withRouter} from "react-router";
+import {connect} from "react-redux";
+import {Switch, Route} from 'react-router-dom';
+import {getSolutions} from '~/app/actions/solution-actions';
+
+import SolutionsList from '~/app/layouts/solutions-list';
+import SolutionPage from '~/app/components/content/solution-page/solution-page';
+
+class Routes extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(getSolutions());
+    }
+
+    render() {
+        console.log('render routes')
+        return (
+                <Switch>
+                    <Route exact path="/"
+                           render={() => <SolutionsList title='HTML/CSS/JS' solutions={this.props.solutions.htmlCssJs}/>}/>
+                    <Route path="/javascript"
+                           render={() => <SolutionsList title='Javascript' solutions={this.props.solutions.javascript}/>}/>
+                    <Route path="/wp"
+                           render={() => <SolutionsList title='Wordpress' solutions={this.props.solutions.wordpress}/>}/>
+                    <Route path="/joomla"
+                           render={() => <SolutionsList title='Joomla' solutions={this.props.solutions.joomla}/>}/>
+                    <Route path="/drupal"
+                           render={() => <SolutionsList title='Drupal' solutions={this.props.solutions.drupal}/>}/>
+                    <Route path="/demo"
+                           render={() => <SolutionPage title='Drupal' solutions={this.props.solutions.solutions}/>}/>
+                </Switch>
+        );
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        solutions: state.solutions
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Routes));
--- src/app/layouts/index-content.js
+++ src/app/layouts/index-content.js
@@ -1,46 +1,19 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {Redirect, withRouter} from 'react-router';
-import {connect} from 'react-redux';
-import {getSolutions} from '~/app/actions/solution-actions';
+import {Redirect} from 'react-router';
 
-import SolutionsList from '~/app/layouts/solutions-list';
-import SolutionPage from '~/app/components/content/solution-page/solution-page';
+import Routes from '~/app/app-routes';
 
 class Index extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(getSolutions());
-  }
+    render() {
+        const redirect = (window.location.hash) ? <Redirect to={window.location.hash.slice(1)}/> : '';
 
-  render() {
-    const redirect = (window.location.hash) ? <Redirect to={window.location.hash.slice(1)}/> : '';
-
-    return (
-      <div className="container">
-        {redirect}
-        <Switch>
-          <Route exact path="/"
-                 render={() => <SolutionsList title='HTML/CSS/JS' solutions={this.props.solutions.htmlCssJs}/>}/>
-          <Route path="/javascript"
-                 render={() => <SolutionsList title='Javascript' solutions={this.props.solutions.javascript}/>}/>
-          <Route path="/wp"
-                 render={() => <SolutionsList title='Wordpress' solutions={this.props.solutions.wordpress}/>}/>
-          <Route path="/joomla"
-                 render={() => <SolutionsList title='Joomla' solutions={this.props.solutions.joomla}/>}/>
-          <Route path="/drupal"
-                 render={() => <SolutionsList title='Drupal' solutions={this.props.solutions.drupal}/>}/>
-          <Route path="/demo"
-                 render={() => <SolutionPage title='Drupal' solutions={this.props.solutions.solutions}/>}/>
-        </Switch>
-      </div>
-    )
-  }
+        return (
+            <div className="container">
+                {redirect}
+                <Routes/>
+            </div>
+        )
+    }
 }
 
-function mapStateToProps(state) {
-  return {
-    solutions: state.solutions
-  }
-}
-
-export default withRouter(connect(mapStateToProps)(Index));
+export default Index;
