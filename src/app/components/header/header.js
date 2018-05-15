0b0ee05d3453a5cf2ada866396513ef915054f2f
tskon <tsekhmeister.k@gmail.com>
2018-05-15 21:01:38 +0300

    replace login

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -3,23 +3,6 @@ import React from 'react';
 export default class Header extends React.Component {
 
   render() {
-    let login;
-    if (this.props.user.user !== '') {
-      login = (
-        <div className="login-block">
-          <h3>{this.props.user.user.givenName || this.props.user.user.displayName}</h3>
-          <a className="login-block__link" href="/admin">Админ-панель</a>
-          <a className="login-block__link" href="/logout">Выйти</a>
-        </div>
-      );
-    } else {
-      login = (
-        <div className="login-block">
-          <a className="login-block__login" href="/auth/google">Google login</a>
-        </div>
-      )
-    }
-
     let menu = '';
     if(this.props.menu) menu = this.props.menu;
 
@@ -30,7 +13,6 @@ export default class Header extends React.Component {
           <h1 className="title__name"><a href='/'>Bredik</a></h1>
           <p className="title__description">Сборник готовых решений для разработчика</p>
         </div>
-        {login}
       </header>
     )
   }
--- src/app/components/menu/main-menu.js
+++ src/app/components/menu/main-menu.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import login from 'app/components/'
 
 export default class Menu extends React.Component {
   setActive(href) {
@@ -14,7 +16,7 @@ export default class Menu extends React.Component {
     menu.classList.toggle('sections-menu-burger_active');
   }
 
-  closeMenu(){
+  closeMenu() {
     const menu = document.querySelector('.sections-menu-burger');
     const btn = document.querySelector('.burger-btn');
 
@@ -29,17 +31,37 @@ export default class Menu extends React.Component {
         this.toggleMenu(e.target)
       }} title={'Меню'}><span/></button>;
 
+    let login;
+    if (this.props.user.user !== '') {
+      login = (
+        <div className="login-block">
+          <h3>{this.props.user.user.givenName || this.props.user.user.displayName}</h3>
+          <a className="login-block__link" href="/admin">Админ-панель</a>
+          <a className="login-block__link" href="/logout">Выйти</a>
+        </div>
+      );
+    } else {
+      login = (
+        <div className="login-block">
+          <a className="login-block__login" href="/auth/google">Google login</a>
+        </div>
+      )
+    }
+
     return (
       <div>
         {burgerBtn}
         <nav className={(this.props.burger) ? "sections-menu-burger" : "sections-menu"}
-             onClick={(this.props.burger) ? this.closeMenu : () => {}}>
+             onClick={(this.props.burger) ? this.closeMenu : () => {
+             }}>
           <Link className={"sections-menu__item " + this.setActive("/")} to="/">HTML/CSS/JS</Link>
-          <Link className={"sections-menu__item " + this.setActive("/javascript")} to="/javascript">JavaScript</Link>
+          <Link className={"sections-menu__item " + this.setActive("/javascript")}
+                to="/javascript">JavaScript</Link>
           <Link className={"sections-menu__item " + this.setActive("/wp")} to="/wp">Wordpress</Link>
           <Link className={"sections-menu__item " + this.setActive("/joomla")} to="/joomla">Joomla</Link>
           <Link className={"sections-menu__item " + this.setActive("/drupal")} to="/drupal">Drupal</Link>
         </nav>
+        {login}
       </div>
     )
   }
