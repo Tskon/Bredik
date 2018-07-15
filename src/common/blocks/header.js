7d6f9b47eed3f1ae2687d9ce093338dfe37597a9
Tskon <tsekhmeister.k@gmail.com>
2018-07-15 20:21:52 +0300

    add Header component

---
+++ src/common/blocks/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component{
+  render(){
+    return (
+      <header>
+        <h1>Bredik</h1>
+        <p>Test page</p>
+      </header>
+    )
+  }
+}
