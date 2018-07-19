a01dd14d640390092ccb89609b4675c6a153346d
Tskon <tsekhmeister.k@gmail.com>
2018-07-19 21:28:08 +0300

    add vore structure files

---
+++ src/common/blocks/sideMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SideMenu extend React.Component{
+  render(){
+    return (
+      <ul>
+        <li>Первая статья</li>
+        <li>Вторая статья</li>
+        <li>Третья статья</li>
+      </ul>
+    )
+  }
+}
+
+export default SideMenu;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 
 import Header from './common/blocks/header';
 
-ReactDom.render(<Header/>, document.getElementById('mainApp'));
+ReactDom.render(<BrowserRouter> </BrowserRouter>, document.getElementById('mainApp'));
 
