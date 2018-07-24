0d9ebefa82e6a590072bbacb99c3d55c9200958c
Tskon <tsekhmeister.k@gmail.com>
2018-07-24 23:09:17 +0300

    change structure. Add new Layout

--- src/layouts/indexPage.js
+++ src/layouts/indexPage.js
@@ -1,14 +1,14 @@
-import Header from 'common/blocks/header';
-import SideMenu from 'common/blocks/sideMenu';
-
 import React from 'react';
+
+import LeftBox from 'layouts/leftBox';
+import Header from 'common/blocks/header';
 
 class IndexPage extends React.Component{
   render(){
     return (
       <main className="container">
         <Header/>
-        <SideMenu/>
+        <LeftBox/>
       </main>
     )
   }
---
+++ src/layouts/leftBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SideMenu from 'common/blocks/sideMenu';
+
+class leftBox extends React.Component{
+  render(){
+    return(
+      <div>
+        <SideMenu/>
+      </div>
+    );
+  }
+}
