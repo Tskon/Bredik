dc05e2a31a22f0b6009daf00039aafa24be87a40
Tskon <tsekhmeister.k@gmail.com>
2018-07-24 23:04:52 +0300

    fix layout name

--- src/core/router.js
+++ src/core/router.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Route, Redirect} from 'react-router-dom';
 import {BrowserRouter} from 'react-router-dom'
 
-import IndexPage from 'layouts/indePage';
+import IndexPage from 'layouts/indexPage';
 
 export default (
   <BrowserRouter>
