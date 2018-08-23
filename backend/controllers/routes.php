2e1ca10efd7203a7e560458c7614f66845e014ca
Tskon <tsekhmeister.k@gmail.com>
2018-08-23 20:48:33 +0300

    fix correct post

--- backend/controllers/routes.php
+++ backend/controllers/routes.php
@@ -3,20 +3,21 @@ require_once $PATH_B . '/models/general.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    switch ($uri){
-        case 'artticle-get-list':
-            echo json_encode(getArticlesList());
-            exit;
-    }
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
   switch ($uri){
-    case 'login':
+    case '/login':
       print 'login page';
       break;
     default:
       renderPage($PATH_B . '/templates/mainPage.php');
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+  switch ($uri){
+    case '/article-get-list':
+      echo json_encode(getArticlesList());
+      exit;
   }
 }
