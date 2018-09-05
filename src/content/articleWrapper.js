1707c2d64ec3d16ffc91b52b86ac30fd615d7ffd
Tskon <tsekhmeister.k@gmail.com>
2018-09-05 17:51:52 +0300

    change article wrapper logic

--- src/content/articleWrapper.js
+++ src/content/articleWrapper.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
 class ArticleWrapper extends React.Component {
-  render(){
-    return <div></div>
+  constructor() {
+    super();
+    this.state = {
+      articleComponents: []
+    }
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getArticlesList());
+
+  }
+
+  componentDidUpdate() {
+    if (this.props.articlesList && !this.state.articleComponents.length) {
+      this.props.articlesList.forEach(article => {
+        const url = `content/articles/${article}`;
+        import('content/articles/mutationObserver.js').then(Component => {
+          this.setState({
+            articleComponents: [...this.state.articleComponents, Component]
+          })
+        })
+      })
+    }
+  }
+
+  render() {
+    return <div>
+      {this.state.articleComponents && this.state.articleComponents.map((article, i) => {
+        const Component = Loadable({
+          loader: () => import('../content/articles/onwheelArticle'),
+          loading: () => {
+            return <div>Loading...</div>
+          },
+        });
+
+        return (
+          <div>
+            <h2>{article}</h2>
+            {Component}
+          </div>)
+      })
+      }
+    </div>
   }
 }
 
-export default  ArticleWrapper
+export default connect((store) => {
+  return {
+    articlesList: store.article.articlesList,
+  }
+})(ArticleWrapper);
