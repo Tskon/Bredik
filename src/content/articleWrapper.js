d53dd4f2247c3aeff8a909ba676798f672d737dd
tskon <tsekhmeister.k@gmail.com>
2018-09-12 19:52:10 +0300

    tests with async component

--- src/content/articleWrapper.js
+++ src/content/articleWrapper.js
@@ -14,35 +14,38 @@ class ArticleWrapper extends React.Component {
 
   componentDidMount() {
     this.props.dispatch(getArticlesList());
+    import('content/articles/mutationObserver.js').then(Component => {
+      this.Component = Component;
+      })
 
   }
 
   componentDidUpdate() {
-    if (this.props.articlesList && !this.state.articleComponents.length) {
-      this.props.articlesList.forEach(article => {
-        const url = `content/articles/${article}`;
-        import('content/articles/mutationObserver.js').then(Component => {
-          this.setState({
-            articleComponents: [...this.state.articleComponents, Component]
-          })
-        })
-      })
-    }
+    // if (this.props.articlesList && !this.state.articleComponents.length) {
+    //   this.props.articlesList.forEach(article => {
+    //     const url = `content/articles/${article}`;
+    //     import('content/articles/mutationObserver.js').then(Component => {
+    //       this.setState({
+    //         articleComponents: [...this.state.articleComponents, Component]
+    //       })
+    //     })
+    //   })
+    // }
   }
 
   render() {
-    console.log(this.state)
-    const Component = Loadable({
-      loader: () => import('../content/articles/onwheelArticle'),
-      loading: () => {
-        return <div>Loading...</div>
-      },
-    });
+    console.log(this.Component)
+    // const Component = Loadable({
+    //   loader: () => import('./articles/onwheelArticle'),
+    //   loading: () => {
+    //     return <div>Loading...</div>
+    //   },
+    // });
     return <div>
       {this.state.articleComponents &&
       <div>
         <h2>Заголовок</h2>
-        {Component}
+        {this.Component}
       </div>
       }
     </div>
