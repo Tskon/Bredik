ac0cc619f7229695e3501df4e5860f73fc0f7f0b
tskon <tsekhmeister.k@gmail.com>
2018-09-13 22:38:52 +0300

    change render logic

--- src/common/blocks/sideMenu.js
+++ src/common/blocks/sideMenu.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-class SideMenu extends React.Component{
-  render(){
+import {getArticlesList} from 'redux/actions/article';
+
+class SideMenu extends React.Component {
+  componentDidMount(){
+    this.props.dispatch(getArticlesList())
+  }
+
+  render() {
     return (
       <div>
         <ul className="sideMenu">
-          {this.props.articlesList && this.props.articlesList.map((article, i)=>{
+          {this.props.articlesList && this.props.articlesList.map((article, i) => {
             const articleName = article.split('.')[0];
-            return <li key={'article_' + i}><Link to={articleName}>{articleName}</Link></li>
+            return <li key={'article_' + i}><Link to={'/article/' + ++i}>{articleName}</Link></li>
           })}
         </ul>
       </div>
@@ -17,7 +23,7 @@ class SideMenu extends React.Component{
   }
 }
 
-export default connect((store)=>{
+export default connect((store) => {
   return {
     articlesList: store.article.articlesList,
   }
--- src/content/articleWrapper.js
+++ src/content/articleWrapper.js
@@ -8,7 +8,7 @@ class ArticleWrapper extends React.Component {
     super();
     this.state = {
       articleComponents: [],
-      currentArticle: null
+      currentArticleIndex: 0,
     }
   }
 
@@ -18,10 +18,15 @@ class ArticleWrapper extends React.Component {
 
   componentDidUpdate(){
     if (this.props.articlesList && !this.state.currentArticle) {
-      import(`content/articles/${this.props.articlesList[0]}`).then(Component => {
+      import(`content/articles/${this.props.articlesList[this.state.currentArticleIndex]}`).then(Component => {
         this.setState({currentArticle: Component});
-      })
+      });
     }
+
+    const currentArticleIndex = document.location.pathname.split('/')[2] - 1 || 0;
+    console.log(this.state.currentArticleIndex,currentArticleIndex)
+    if (this.state.currentArticleIndex !== currentArticleIndex) this.setState({currentArticleIndex: currentArticleIndex});
+
   }
 
   render() {
