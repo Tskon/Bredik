444de43fbcd5514b3595bdb420a6f68c21b41e5e
Tskon <tsekhmeister.k@gmail.com>
2018-09-17 21:37:33 +0300

    add link to main page

--- src/common/blocks/header.js
+++ src/common/blocks/header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Header extends React.Component{
   render(){
     return (
       <header className="mainHeader ">
-        <h1>Bredik</h1>
+        <h1><Link to="/">Bredik</Link></h1>
         <p>Test page</p>
       </header>
     )
