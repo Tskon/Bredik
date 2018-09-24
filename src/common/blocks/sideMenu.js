7dd1dcc8cd9c26eb95db895c478274878bcece11
Tskon <tsekhmeister.k@gmail.com>
2018-09-24 20:34:44 +0300

    add custom names for articles

--- src/common/blocks/sideMenu.js
+++ src/common/blocks/sideMenu.js
@@ -5,7 +5,17 @@ import {connect} from 'react-redux';
 import {getArticlesList} from 'redux/actions/article';
 
 class SideMenu extends React.Component {
-  componentDidMount(){
+  constructor() {
+    super();
+    this.state = {
+      articleTitle: {
+        'mutationObserver': 'Плохое API сторонних библиотек не приговор!',
+        'onwheelArticle': 'Подводные камни при работе со скроллом'
+      }
+    }
+  }
+
+  componentDidMount() {
     this.props.dispatch(getArticlesList())
   }
 
@@ -15,7 +25,8 @@ class SideMenu extends React.Component {
         <ul className="sideMenu">
           {this.props.articlesList && this.props.articlesList.map((article, i) => {
             const articleName = article.split('.')[0];
-            return <li key={'article_' + i}><Link to={'/article/' + ++i}>{articleName}</Link></li>
+            return <li key={'article_' + i}><Link to={'/article/' + ++i}>{this.state.articleTitle[articleName] ?
+              this.state.articleTitle[articleName] : articleName}</Link></li>
           })}
         </ul>
       </div>
