07770e14185db8817f79c64a20ce8f14a3ee63a9
Tskon <tsekhmeister.k@gmail.com>
2018-10-20 13:32:11 +0300

    add article creator

---
+++ src/pages/ArticleCreator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class ArticleCreator extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <form onSubmit={}>
+        <div>
+          <label htmlFor="articleTitle">Название статьи</label>
+          <input type="articleTitle"/>
+        </div>
+        <div>
+          <label htmlFor="articleBody">Текст статьи</label>
+          <textarea name="articleBody" id="" cols="30" rows="10">
+            sdsdsdsd
+          </textarea>
+        </div>
+      </form>
+    );
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+  }
+}
+
+export default ArticleCreator;
