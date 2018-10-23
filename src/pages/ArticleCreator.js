472b5ee1b8012d0e75aaa79a48f136e734006b9a
Tskon <tsekhmeister.k@gmail.com>
2018-10-23 20:35:01 +0300

    few content changes

--- src/pages/ArticleCreator.js
+++ src/pages/ArticleCreator.js
@@ -10,15 +10,15 @@ class ArticleCreator extends React.Component{
       <form onSubmit={this.onSubmit}>
         <div>
           <label htmlFor="articleTitle">Название статьи</label>
-          <input type="articleTitle"/>
+          <input type="articleTitle" placeholder='Название статьи'/>
         </div>
         <div>
           <label htmlFor="articleBody">Текст статьи</label>
           <textarea name="articleBody" id="" cols="30" rows="10">
-            sdsdsdsd
+
           </textarea>
         </div>
-        <button>отправить</button>
+        <button>Отправить</button>
       </form>
     );
   }
